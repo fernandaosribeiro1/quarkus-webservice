4a87d8b1cc30d9e68b34f51a3b101e4812204d5d
fernanda.osribeiro1 <fernanda.osribeiro1@gmail.com>
2025-08-12 21:51:19 -0300

    Initial Commit

---
+++ src/main/java/org/senac/GreetingResource.java
@@ -0,0 +1,48 @@
+package org.senac;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public MyEntity hello() {
+        return new MyEntity("Hello from Quarkus REST");
+    }
+
+    @GET
+    @Path("/GetAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MyEntity> getAll() {
+        return MyEntity.listAll();
+    }
+
+    @GET
+    @Path("/GetAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MyEntity> GetAll(){
+        return MyEntity.listAll();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void insertEntity(MyEntity entity) {
+        MyEntity.persist(entity);
+    }
+
+    @DELETE
+    @Path ("{id")
+    @Transactional
+    @Consumes
+    public void delete (@PathParam("id")int id){
+        MyEntity.deleteById(id);
+    }
+
+
+}
